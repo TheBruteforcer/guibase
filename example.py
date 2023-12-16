7c3a4e4fcb6965b87d760882d903204ed8722210
Omar Ellaban <omar.ellaban00@gmail.com>
2023-12-16 23:30:45 +0200

    Update example.py

--- example.py
+++ example.py
@@ -1,11 +1,11 @@
-# Import module
+# Import module.
 from guibase import *
-# Create 2 functions to set as Failure / Success actions
+# Create 2 functions to set as Failure / Success actions.
 def Faliure(msg): # msg is required.
     print('Cannot Login / Signup.')
     print(msg)
 def Success():
     print('Signed in successfully.')
-
+# Create GUI.
 guilogin(key = "! HTTP FIREBASE KEY !", title = "Sign in to complete", whensuccess=Success, whenfaliure=Faliure, lang = 'en')
 guiregister(key = "! HTTP FIREBASE KEY !", title = "Sign up to complete", whensuccess=Success, whenfaliure=Faliure, lang = 'en')
